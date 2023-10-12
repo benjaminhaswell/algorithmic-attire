70ff9b8d7eb4dfc6f9b301febe07698987d29d49
Ben Haswell <haswelba@miamioh.edu>
2023-10-11 21:18:27 -0400

    products.js formatting - issue still unresolved

--- app/api/products.js
+++ app/api/products.js
@@ -1,7 +1,12 @@
+console.log("GETS INTO PRODUCTS");
+
 import { initMongoose } from "@/lib/mongoose";
 import Product from "../../models/Product";
 
 export default async function handle(req, res) {
+
+    console("gets into products");
+
     await initMongoose();
 
     res.json(await Product.find().exec());
